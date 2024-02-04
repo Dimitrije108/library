82b81af4dbdcbd1065ab0967212e82f0c2dcce47
Dimitrije Markovic <126205228+Dimitrije108@users.noreply.github.com>
2024-02-04 16:07:41 +0100

    Add new book functionality via user form input
    Submiting user form will construct a new book object and add it to myLibrary array

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const modal = document.querySelector('.modal');
 const openModal = document.querySelector('.add-button');
 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const image = document.querySelector('#image');
+const read = document.querySelector('#read');
+const submitBtn = document.querySelector('.submitBtn');
+
+let userTitle;
+let userAuthor;
+let userPages;
+let userImage;
+let userRead;
+
+let newBook;
+
 const myLibrary = [];
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+// Close modal if a click is registered outside of the modal box
+
+modal.addEventListener('click', (e) => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
+})
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,26 +48,22 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    
+    userTitle = title.value;
+    userAuthor = author.value;
+    userPages = pages.value;
+    userImage = image.value;
+    userRead = read.value;
+
+    newBook = new Book(userTitle, userAuthor, userPages, userImage, userRead);
+    myLibrary.push(newBook);
 }
 
-openModal.addEventListener('click', () => {
-    modal.showModal();
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
 })
 
-// Close modal if a click is registered outside of it
 
-modal.addEventListener('click', e => {
-    const dialogDimensions = modal.getBoundingClientRect()
-    if (
-        e.clientX < dialogDimensions.left ||
-        e.clientX > dialogDimensions.right ||
-        e.clientY < dialogDimensions.top ||
-        e.clientY > dialogDimensions.bottom
-    ) {
-        modal.close();
-    }
-})
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
 
