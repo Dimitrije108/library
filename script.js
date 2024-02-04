6477ce16a339d22a4b7852389807d9bc5ebd112a
Dimitrije Markovic <126205228+Dimitrije108@users.noreply.github.com>
2024-02-04 14:04:05 +0100

    Add html modal form element for adding book information and javascript showModal and close modal feature when user clicks outside the modal box

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.add-button');
+
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -6,8 +11,40 @@ function Book(title, author, pages, read) {
     this.info = function() {
         console.log(this.title + " by " + this.author + ", " + this.pages + ", " + this.read);
     }
+
 }
+
+function addBookToLibrary() {
+    
+}
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+// Close modal if a click is registered outside of it
+
+modal.addEventListener('click', e => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
+})
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
 
+const theLastKingdom = new Book('The Last Kingdom', 'Bernard Cornwell', '352 pages', 'read');
+
+const bladeRunner = new Book('Do Androids Dream of Electric Sheep?', 'Philip K. Dick', '210 pages', 'not read yet');
+
+const affairAtStyles = new Book('The Mysterious Affair at Styles', 'Agatha Christie', '296 pages', 'not read yet');
+
 theHobbit.info();
+theLastKingdom.info();
+bladeRunner.info();
+affairAtStyles.info();
